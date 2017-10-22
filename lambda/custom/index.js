13d847cda6540f587f41359c9447e9ff8d264ecf
Mark Tucker <mtucke13@asu.edu>
2017-10-21 23:07:21 -0700

    Added logging, intentMap, and used more 'ask' responses

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -7,6 +7,17 @@
 const app = require('jovo-framework').Jovo;
 
 exports.handler = function(event, context, callback) {
+
+    app.enableRequestLogging();
+    app.enableResponseLogging();
+
+    let intentMap = {
+        'AMAZON.HelpIntent' : 'HelpIntent',
+        'AMAZON.StopIntent' : 'StopIntent',
+        'AMAZON.CancelIntent' : 'StopIntent'
+    };
+    app.setIntentMap(intentMap);
+
     app.handleRequest(event, callback, handlers);
     app.execute();
 };
@@ -19,38 +30,34 @@ exports.handler = function(event, context, callback) {
 const handlers = {
 
     'LAUNCH': function() {
-        app.toIntent('HelloWorldIntent');
+        app.ask('Welcome to Hello World. You can say "Hello World" or "My name is Fred". What would you like to do?', 'For a complete list of options, say "help"');
     },
 
     'END' : function() {
         let reason = app.getEndReason();
         console.log('Session ended with reason: ' + reason);
 
-        app.tell('Bye!');        
+        app.tell('goodbye!');        
     },    
 
-    'AMAZON.StopIntent' : function() {
+    'StopIntent' : function() {
         app.toIntent('END');
     },
 
-    'AMAZON.CancelIntent' : function() {
-        app.toIntent('END');
-    },
-
-    'AMAZON.HelpIntent' : function() {
-        app.tell('You can try: "alexa, hello world" or "alexa, ask hello world my name is John"');
+    'HelpIntent' : function() {
+        app.ask('You can try: "hello world" or "my name is John". Which do you want to do?', 'For a complete list of options, say "help"');
     },
     
     'HelloWorldIntent': function() {
-        app.tell('Hello World!');
+        app.ask('Hello big beautiful world! What do you want to do next?', 'For a complete list of options, say "help"');
     },
 
     'MyNameIsIntent': function() {
         let name = app.getInput('name');
-        app.tell('Hello there, ' + name);
+        app.ask('Hello there, ' + name + '. What do you want to do now?', 'For a complete list of options, say "help"');
     },
     
     'Unhandled' : function() {
-        app.tell("Sorry, I didn't get that. Can you please repeat?");
+        app.ask("Sorry, I didn't get that. Can you please repeat?", 'For a complete list of options, say "help"');
     }
 };
